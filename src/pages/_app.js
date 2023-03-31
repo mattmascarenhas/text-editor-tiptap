0b48b7e88fa9982611b02aeff1ff1092b0fb5ed7
Matheus Mascarenhas <mattmascarenhas7@gmail.com>
2023-03-31 18:47:12 -0300

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@/styles/styles.scss";
+import "@/styles/MenuBar.scss";
+import "@/styles/MenuItem.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
